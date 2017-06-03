518fa14b24d2a52a93090f292bf3f03090828353
ant3m <stan.gomes94@gmail.com>
2017-06-02 23:36:01 -0400

    Basic Layout

--- app/src/main/java/com/stan/moonshine/MainActivity.java
+++ app/src/main/java/com/stan/moonshine/MainActivity.java
@@ -3,10 +3,14 @@ package com.stan.moonshine;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    //just a commit test
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         String apiKey = "91aa6458ac098dc8ae421cbda8510785";
         double latitude = 45.3167088;
         double longitude = -75.83175890000001;
-        String forecastUrl = "https://api.darksky.net/forecast/"+ apiKey +"/"+ latitude +","+longitude;
+        String forecastUrl = "https://api.darksky.net/forecast/"+ apiKey +"/"+ latitude +","+longitude+"?units=si";
 
         if (isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
@@ -39,20 +43,25 @@ public class MainActivity extends AppCompatActivity {
             Call call = client.newCall(request);
             call.enqueue(new Callback() {
                 @Override
-                public void onFailure(Call call, IOException e) {
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
 
                 }
 
                 @Override
-                public void onResponse(Call call, Response response) throws IOException {
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                     try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-                            Log.v(TAG, response.body().string());
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
                             showErrorDialog();
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e){
+                        Log.e(TAG,"Exception caught ", e);
                     }
                 }
             });
@@ -60,6 +69,31 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(this,"Network is unavailable!",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG,"From JSON: "+timezone);
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTemp(currently.getDouble("temperature"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
+
+
     }
 
     private boolean isNetworkAvailable() {
